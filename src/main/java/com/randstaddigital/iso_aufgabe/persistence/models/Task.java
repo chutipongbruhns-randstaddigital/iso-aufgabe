b2f9066cb5f10397a05a8099c2e721eb1e3df513
Pan <hypersausage@web.de>
2025-09-08 16:38:16 +0200

    feat: Implementing Model Class and Repository Class

---
+++ src/main/java/com/randstaddigital/iso_aufgabe/persistence/models/Task.java
@@ -0,0 +1,23 @@
+package com.randstaddigital.iso_aufgabe.persistence.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed = false;
+}
