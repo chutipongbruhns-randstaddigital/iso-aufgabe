4bce0b38f12b51f751a00714d0650cfbddb40ddc
Pan <hypersausage@web.de>
2025-09-08 17:48:10 +0200

    chore: optimized imports

--- src/test/java/com/randstaddigital/iso_aufgabe/services/TaskServiceTest.java
+++ src/test/java/com/randstaddigital/iso_aufgabe/services/TaskServiceTest.java
@@ -11,7 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 
