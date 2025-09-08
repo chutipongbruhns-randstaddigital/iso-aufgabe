b259969957722a237174ea4800e4d52381238d85
Pan <hypersausage@web.de>
2025-09-08 16:22:38 +0200

    chore: Initial Setup

---
+++ src/main/java/com/randstaddigital/iso_aufgabe/IsoAufgabeApplication.java
@@ -0,0 +1,13 @@
+package com.randstaddigital.iso_aufgabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoAufgabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsoAufgabeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randstaddigital/iso_aufgabe/IsoAufgabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randstaddigital.iso_aufgabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsoAufgabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
